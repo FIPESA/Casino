20eaf488f212c9b981f04ec357ad45dc2f30e800
alumno <alumno@A12101>
2016-06-09 14:08:54 +0200

    Git Arreglado

--- Casino/src/Juegos/Slots.java
+++ Casino/src/Juegos/Slots.java
@@ -10,7 +10,7 @@ import java.util.*;
 
 public class Slots{
     
-    public final static int CANTIDAD_NUMEROS = 45;
+    public final static int CANTIDAD_NUMEROS = 9;
     public final static int TRIPLESIETE = 1;
     public final static int PLATANO = 2;
     public final static int SANDIA = 3;
@@ -34,7 +34,7 @@ public class Slots{
      */
     public static int aleatorio(){
         Random aleatorio = new Random();
-        return aleatorio.nextInt(CANTIDAD_NUMEROS + 1);        
+        return aleatorio.nextInt(CANTIDAD_NUMEROS);        
     }
     
     /**
@@ -44,65 +44,65 @@ public class Slots{
         int aleatorio;
         
         aleatorio = aleatorio();
-        if(aleatorio>=0 && aleatorio<10 ){
+        if(aleatorio==0){
              resultado1 = CEREZA;
-        }else if(aleatorio>=10 && aleatorio<18){
+        }else if(aleatorio==1){
              resultado1 = UVA;
-        }else if(aleatorio>=18 && aleatorio<25){
+        }else if(aleatorio==2){
              resultado1 = NARANJA;
-        }else if(aleatorio>=25 && aleatorio<31){
+        }else if(aleatorio==3){
              resultado1 = LIMON;
-        }else if(aleatorio>=31 && aleatorio<36){
+        }else if(aleatorio==4){
              resultado1 = BAR;
-        }else if(aleatorio>=36 && aleatorio<40){
+        }else if(aleatorio==5){
              resultado1 = CAMPANA;
-        }else if(aleatorio>=40 && aleatorio<43){
+        }else if(aleatorio==6){
              resultado1 = SANDIA;
-        }else if(aleatorio>=43 && aleatorio<45){
+        }else if(aleatorio==7){
              resultado1 = PLATANO;
-        }else if(aleatorio==45 ){
+        }else if(aleatorio==8){
              resultado1 = TRIPLESIETE;
         }
         
         aleatorio = aleatorio();
-        if(aleatorio>=1 && aleatorio<15 ){
+        if(aleatorio==0){
              resultado2 = CEREZA;
-        }else if(aleatorio>=15 && aleatorio<27){
+        }else if(aleatorio==1){
              resultado2 = UVA;
-        }else if(aleatorio>=27 && aleatorio<38){
+        }else if(aleatorio==2){
              resultado2 = NARANJA;
-        }else if(aleatorio>=38 && aleatorio<47){
+        }else if(aleatorio==3){
              resultado2 = LIMON;
-        }else if(aleatorio>=47 && aleatorio<55){
+        }else if(aleatorio==4){
              resultado2 = BAR;
-        }else if(aleatorio>=55 && aleatorio<62){
+        }else if(aleatorio==5){
              resultado2 = CAMPANA;
-        }else if(aleatorio>=62 && aleatorio<68){
+        }else if(aleatorio==6){
              resultado2 = SANDIA;
-        }else if(aleatorio>=68 && aleatorio<70){
+        }else if(aleatorio==7){
              resultado2 = PLATANO;
-        }else if(aleatorio==70 ){
+        }else if(aleatorio==8){
              resultado2 = TRIPLESIETE;
         }
         
         aleatorio = aleatorio();
-        if(aleatorio>=1 && aleatorio<15 ){
+        if(aleatorio==0){
              resultado3 = CEREZA;
-        }else if(aleatorio>=15 && aleatorio<27){
+        }else if(aleatorio==1){
              resultado3 = UVA;
-        }else if(aleatorio>=27 && aleatorio<38){
+        }else if(aleatorio==2){
              resultado3 = NARANJA;
-        }else if(aleatorio>=38 && aleatorio<47){
+        }else if(aleatorio==3){
              resultado3 = LIMON;
-        }else if(aleatorio>=47 && aleatorio<55){
+        }else if(aleatorio==4){
              resultado3 = BAR;
-        }else if(aleatorio>=55 && aleatorio<62){
+        }else if(aleatorio==5){
              resultado3 = CAMPANA;
-        }else if(aleatorio>=62 && aleatorio<68){
+        }else if(aleatorio==6){
              resultado3 = SANDIA;
-        }else if(aleatorio>=68 && aleatorio<70){
+        }else if(aleatorio==7){
              resultado3 = PLATANO;
-        }else if(aleatorio==70 ){
+        }else if(aleatorio==8 ){
              resultado3 = TRIPLESIETE;
         }
     }
