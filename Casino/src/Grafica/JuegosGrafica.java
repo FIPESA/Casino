a59d707851ec7b9aad84cb8315ad160c5d940c38
alumno <alumno@A12102>
2016-06-09 14:09:07 +0200

    Trabajo hoy paco

--- Casino/src/Grafica/JuegosGrafica.java
+++ Casino/src/Grafica/JuegosGrafica.java
@@ -7,7 +7,10 @@ package Grafica;
 
 import Casino.Casino;
 import Juegos.BlackJackGraf.JugarBlackJack;
+import Usuarios.F2P;
 import Usuarios.User;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 /**
@@ -19,6 +22,9 @@ public class JuegosGrafica extends javax.swing.JFrame {
     
     private Casino fipesa;
     private JFrame root;
+    private ImageIcon f2p = new ImageIcon(getClass().getResource("/resources/imagenesusers/f2pedit.png"));
+    //private ImageIcon p2w = new ImageIcon(getClass().getResource("/resources/imagenesusers/p2wedit.png"));
+   // private ImageIcon p2wss = new ImageIcon(getClass().getResource("/resources/imagenesusers/p2wssedit.png"));
     
     
     public JuegosGrafica(Casino fipesa,JFrame root) {
@@ -27,7 +33,9 @@ public class JuegosGrafica extends javax.swing.JFrame {
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.fipesa = fipesa;
         this.root = root;
-
+        jTextField1.setText(fipesa.getUsuario().getUsername());
+        jTextField2.setText(Double.toString(fipesa.getUsuario().getMonedero().getFondos()));
+        jLabel5.setIcon(f2p);
     }
 
     /**
@@ -39,78 +47,174 @@ public class JuegosGrafica extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         jBrul = new javax.swing.JButton();
         jBBj = new javax.swing.JButton();
         jBSlots = new javax.swing.JButton();
         jBatras = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
-        jLabel4 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        getContentPane().setLayout(null);
+        setPreferredSize(new java.awt.Dimension(800, 600));
 
-        jLabel1.setFont(new java.awt.Font("Times New Roman", 3, 36)); // NOI18N
-        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\alumno\\Desktop\\juegos-casinos_logo (1).jpg")); // NOI18N
-        getContentPane().add(jLabel1);
-        jLabel1.setBounds(550, 10, 327, 120);
+        jPanel1.setBackground(new java.awt.Color(204, 204, 255));
+        jPanel1.setOpaque(false);
+        jPanel1.setLayout(null);
 
         jLabel2.setFont(new java.awt.Font("Matura MT Script Capitals", 1, 36)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(204, 0, 0));
         jLabel2.setText("Seleccione un juego: ");
-        getContentPane().add(jLabel2);
-        jLabel2.setBounds(550, 140, 367, 49);
+        jPanel1.add(jLabel2);
+        jLabel2.setBounds(230, 50, 370, 40);
 
         jBrul.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
-        jBrul.setIcon(new javax.swing.ImageIcon("C:\\Users\\alumno\\Desktop\\89.jpg")); // NOI18N
         jBrul.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jBrulActionPerformed(evt);
             }
         });
-        getContentPane().add(jBrul);
-        jBrul.setBounds(510, 240, 440, 300);
+        jPanel1.add(jBrul);
+        jBrul.setBounds(220, 180, 80, 80);
 
-        jBBj.setIcon(new javax.swing.ImageIcon("C:\\Users\\alumno\\Desktop\\BLACK.png")); // NOI18N
         jBBj.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jBBjActionPerformed(evt);
             }
         });
-        getContentPane().add(jBBj);
-        jBBj.setBounds(1030, 240, 290, 300);
+        jPanel1.add(jBBj);
+        jBBj.setBounds(330, 180, 80, 60);
 
-        jBSlots.setIcon(new javax.swing.ImageIcon("C:\\Users\\alumno\\Desktop\\51GgLQx6dML._SY355_.jpg")); // NOI18N
         jBSlots.setText("SLOTS");
         jBSlots.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jBSlotsActionPerformed(evt);
             }
         });
-        getContentPane().add(jBSlots);
-        jBSlots.setBounds(120, 180, 310, 380);
+        jPanel1.add(jBSlots);
+        jBSlots.setBounds(120, 180, 80, 90);
 
         jBatras.setFont(new java.awt.Font("Tahoma", 3, 24)); // NOI18N
         jBatras.setText("Atrás");
-        jBatras.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 3, true));
+        jBatras.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jBatras.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jBatrasActionPerformed(evt);
             }
         });
-        getContentPane().add(jBatras);
-        jBatras.setBounds(500, 650, 100, 70);
+        jPanel1.add(jBatras);
+        jBatras.setBounds(30, 440, 80, 60);
 
-        jButton1.setFont(new java.awt.Font("Tahoma", 3, 24)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 0, 0));
-        jButton1.setText("Cerrar Sesión");
-        jButton1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 0, 0), 4, true));
-        getContentPane().add(jButton1);
-        jButton1.setBounds(790, 650, 170, 70);
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255, 200));
 
-        jLabel4.setIcon(new javax.swing.ImageIcon("C:\\Users\\alumno\\Desktop\\fondo-general.jpg")); // NOI18N
-        getContentPane().add(jLabel4);
-        jLabel4.setBounds(-10, -20, 1500, 860);
+        jLabel5.setText("jLabel4");
+
+        jLabel6.setText("Jugando como: ");
+
+        jTextField1.setText("jTextField1");
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+
+        jLabel7.setText("Fondos: ");
+
+        jTextField2.setText("jTextField2");
+
+        jButton2.setText("Monedero");
+        jButton2.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setText("Logout");
+        jButton3.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel7)
+                            .addComponent(jLabel6))
+                        .addGap(41, 41, 41)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jTextField1)
+                            .addComponent(jTextField2)))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
+                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel6)
+                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel7)
+                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jButton2)
+                            .addComponent(jButton3))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+
+        jPanel1.add(jPanel2);
+        jPanel2.setBounds(440, 400, 354, 120);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/Imagenes/fondoSelJuego.png"))); // NOI18N
+        jLabel1.setText("jLabel1");
+        jPanel1.add(jLabel1);
+        jLabel1.setBounds(0, 0, 800, 600);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 800, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 600, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -138,6 +242,26 @@ public class JuegosGrafica extends javax.swing.JFrame {
         this.setVisible(false);
 
     }//GEN-LAST:event_jBSlotsActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        if(fipesa.getUsuario() instanceof F2P){
+            jButton3.setEnabled(false);
+        } else{
+            fipesa.logout();
+            this.setVisible(false);
+            root.setVisible(true);
+        }        
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        VerMonedero vm = new VerMonedero(fipesa,this);
+        vm.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -180,9 +304,16 @@ public class JuegosGrafica extends javax.swing.JFrame {
     private javax.swing.JButton jBSlots;
     private javax.swing.JButton jBatras;
     private javax.swing.JButton jBrul;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
 }
